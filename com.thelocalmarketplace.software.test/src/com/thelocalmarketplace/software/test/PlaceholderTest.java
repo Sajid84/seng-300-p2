217b72878ff0d57f1ff678500497d1ff73bc7596
Sajid84 <102977866+Sajid84@users.noreply.github.com>
2023-11-05 05:17:50 -0700

    Upload software and test projects with placeholder

---
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PlaceholderTest.java
@@ -0,0 +1,15 @@
+package com.thelocalmarketplace.software.test;
+
+import org.junit.Test;
+
+//Just made this as a placeholder class to setup the file structure on the github repo
+//once an actual testing class is added to the repo, this one can be deleted
+
+public class PlaceholderTest {
+
+	@Test
+	public void testSomething() {
+		
+	}
+	
+}
---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PlaceholderClass.java
@@ -0,0 +1,13 @@
+package com.thelocalmarketplace.software;
+
+// Just made this as a placeholder class to setup the file structure on the github repo
+// once an actual control software class is added to the repo, this one can be deleted
+
+public class PlaceholderClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
