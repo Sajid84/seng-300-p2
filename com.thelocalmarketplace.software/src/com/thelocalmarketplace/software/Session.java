090c25b47016ae11cf6a36bed8d5b38954f6a28a
sanaabdelhalem <sana.abdelhalem@ucalgary.ca>
2023-11-19 19:18:08 -0700

    Added UCIDs to the top of some files

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Session.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Session.java
@@ -1,5 +1,16 @@
-//Yang Yang:30156356
+//Yang Yang           30156356
+// Sana Abdelhalem    30163580
+// Ali Al Yasseen     30151000
+// Andres Genatios    30142768
+// Abdullah Ishtiaq   30153185
+// Nicholas MacKinnon 30172737
+// Carlos Serrouya    30192761
+// Logan Miszaniec    30156384
+// Ali Sebbah         30172851
+
 //Edited on the base of Iteration1
+
+
 package com.thelocalmarketplace.software;
 import ca.ucalgary.seng300.simulation.InvalidArgumentSimulationException;
 import ca.ucalgary.seng300.simulation.InvalidStateSimulationException;
