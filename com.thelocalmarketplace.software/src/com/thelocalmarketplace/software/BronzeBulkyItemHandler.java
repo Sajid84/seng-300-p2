5796d89f1a6a585655927194873356c8d9f9f047
sanaabdelhalem <sana.abdelhalem@ucalgary.ca>
2023-11-20 21:50:56 -0700

    Fixed the Bronze Handler class

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/BronzeBulkyItemHandler.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/BronzeBulkyItemHandler.java
@@ -8,6 +8,8 @@
 // Logan Miszaniec    30156384
 // Ali Sebbah         30172851
 
+package com.thelocalmarketplace.software;
+
 import com.jjjwelectronics.scale.ElectronicScaleBronze;
 import com.jjjwelectronics.scanner.BarcodedItem;
 
@@ -21,31 +23,35 @@ public class BronzeBulkyItemHandler {
      * @param scale The electronic scale used to weigh items.
      * @param session The current checkout session.
      */
-     public BronzeBulkyItemHandler(ElectronicScaleBronze scale, Session session) {
-         this.scale = scale;
-         this.session = session;
-     }
+    public BronzeBulkyItemHandler(ElectronicScaleBronze scale, Session session) {
+        this.scale = scale;
+        this.session = session;
+    }
 
-     /**
+    /**
      * Processes a bulky item, adjusting the session accordingly.
      *
      * @param item The barcoded item to be processed.
      */
-     public void processBulkyItem(BarcodedItem item) {
-         // Use the scale instance to get the mass limit
-         if (item.getMass().compareTo(scale.getMassLimit()) > 0) {
-             // The item is too bulky to be weighed on the scale.
-             // Block further customer input and simulate attendant interaction.
-             session.blockStationForBulkyItem();
-             // Adjust the expected weight in the bagging area.
-             session.adjustForBulkyItem(item);
-             // Unblock the station for further customer input.
-             session.unblockStationForBulkyItem();
-             // Provide feedback to the user that the bulky item has been processed.
-             System.out.println("Bulky item processed: " + item.getBarcode());
-         } else {
-             // If the item is not too heavy, process as normal.
-             session.processNormalItem(item);
-         }
-     }
+    public void processBulkyItem(BarcodedItem item) {
+        // Use the scale instance to get the mass limit
+        if (item.getMass().compareTo(scale.getMassLimit()) > 0) {
+            // The item is too bulky to be weighed on the scale.
+            // Block further customer input and simulate attendant interaction.
+            session.blockStationForBulkyItem();
+            
+            // Adjust the expected weight in the bagging area.
+            session.adjustForBulkyItem(item);
+
+            // Unblock the station for further customer input.
+            session.unblockStationForBulkyItem();
+
+            // Provide feedback to the user that the bulky item has been processed.
+            System.out.println("Bulky item processed: " + item.getBarcode());
+        } else {
+            // If the item is not too heavy, process as normal.
+            session.processNormalItem(item);
+        }
+    }
 }
+
