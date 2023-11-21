5dccb10c1304642d0f560baab1540363cf718773
alister <alister@localhost>
2023-11-20 18:40:43 -0700

    Fixed RemoveItem test code

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/removeItemTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/removeItemTest.java
@@ -10,185 +10,215 @@
 // Shaikh Sajid Mahmood 30182396
 
 package com.thelocalmarketplace.software.test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jjjwelectronics.Item;
+import com.jjjwelectronics.Mass;
 import com.jjjwelectronics.Numeral;
 import com.jjjwelectronics.scanner.Barcode;
+import com.jjjwelectronics.scanner.BarcodedItem;
+import com.tdc.DisabledException;
 import com.thelocalmarketplace.hardware.BarcodedProduct;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
+import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
+import com.thelocalmarketplace.hardware.external.ProductDatabases;
+import com.thelocalmarketplace.software.BarcodeListener;
 import com.thelocalmarketplace.software.Cart;
+import com.thelocalmarketplace.software.Session;
 
-import java.util.ArrayList;
-import java.util.List;
+import ca.ucalgary.seng300.simulation.SimulationException;
+import powerutility.NoPowerException;
+import powerutility.PowerGrid;
+
 
 public class removeItemTest {
+	
+	AbstractSelfCheckoutStation station;
+	Session session;
+	PowerGrid pg;
+	public Cart cart;
+	
+	public ProductDatabases database_one_item;
+	public Barcode barcode;
+	public Barcode barcode2;
+	public Numeral digits;
+	
+	public BarcodedItem bitem;
+	public Mass itemMass;
+	
+	public BarcodedItem bitem2;
+	public Mass itemMass2;
+	public BarcodedItem bitem3;
+	public Mass itemMass3;
+	
+	public BarcodedItem bitem4;
+	public Mass itemMass4;
+	public Numeral[] barcode_numeral;
+	public Numeral[] barcode_numeral2;
+	public Numeral[] barcode_numeral3;
+	public Barcode b_test;
+	public BarcodedProduct product;
+	public BarcodedProduct product2;
+	public BarcodedProduct product3;
+	
+	
+	@Before public void setUp() {
+		//d1 = new dummyProductDatabaseWithOneItem();
+		//d2 = new dummyProductDatabaseWithNoItemsInInventory();
+		AbstractSelfCheckoutStation.resetConfigurationToDefaults();
+		station = new SelfCheckoutStationBronze();
+		
+		pg =PowerGrid.instance();
+		pg.engageUninterruptiblePowerSource();
+		
+		//initialize database
+		barcode_numeral = new Numeral[]{digits.one,digits.two, digits.three};
+		barcode_numeral2 = new Numeral[]{digits.three,digits.two, digits.three};
+		barcode_numeral3 = new Numeral[]{digits.three,digits.three, digits.three};
+		barcode = new Barcode(barcode_numeral);
+		barcode2 = new Barcode(barcode_numeral2);
+		b_test = new Barcode(barcode_numeral3);
+		product = new BarcodedProduct(barcode, "some item",(long)5.99,(double)3.0);
+		product2 = new BarcodedProduct(barcode2, "some item 2",(long)1.00,(double)300.0);
+		product3 = new BarcodedProduct(b_test, "some item 3",(long)1.00,(double)3.0);
+		database_one_item.BARCODED_PRODUCT_DATABASE.put(barcode, product);
+		database_one_item.INVENTORY.put(product, 1);
+		database_one_item.BARCODED_PRODUCT_DATABASE.put(barcode2, product2);
+		database_one_item.INVENTORY.put(product2, 1);
+		
 
-    private Cart cart;
+		//initialize barcoded item
+		itemMass = new Mass((long) 1000000);
+		bitem = new BarcodedItem(barcode2, itemMass);
+		itemMass2 = new Mass((double) 300.0);//300.0 grams
+		bitem2 = new BarcodedItem(barcode, itemMass2);
+		itemMass3 = new Mass((double) 3.0);//3.0 grams
+		bitem3 = new BarcodedItem(barcode, itemMass3);
+		bitem4 = new BarcodedItem(b_test, itemMass3);
+	}
+	public void prepareStation()
+	{
+		station.plugIn(pg);
+		station.turnOn();
+		session = new Session(this.station);
+		session.startSession();
+	}
+	
 
-    @Before
-    public void setUp() {
-        // Initialize a new ShoppingCart before each test
-        cart = new Cart();
-    }
+	
+	@Test (expected = SimulationException.class) public void testNoPowerOff(){
+		station.plugIn(pg);
+		station.turnOn();
+		session = new Session(this.station);
+		session.startSession();
+		station.handheldScanner.scan(bitem2);
+}
+	@Test
+	public void testPowerOn(){
+		prepareStation();
+		station.handheldScanner.scan(bitem2);
+		
+		//assertTrue("item was not successfully added to cart", session.cart.getNumberItems() ==1);
+	}
+	
+	 
+	 @Test
+	    public void testRemoveBarcodedProductFromCart() {
+	        // Test removing a BarcodedProduct from a non-empty cart
+	    	Numeral[] numeralArray = {Numeral.one, Numeral.two, Numeral.three, Numeral.four};
+	    	Numeral[] numeralArray1 = {Numeral.two, Numeral.two, Numeral.three, Numeral.four};
+	    	Barcode barcode1 = new Barcode(numeralArray);
+	    	Barcode barcode2 = new Barcode(numeralArray1);
+	        BarcodedProduct product1 = new BarcodedProduct(barcode1, "Product1", 1000, 10.0);
+	      
+	        cart = new Cart();
+	        cart.canChange();
+	        cart.endPayment();
+	        
 
-    @Test
-    public void testRemoveBarcodedProductFromCart() {
-        // Test removing a BarcodedProduct from a non-empty cart
-    	Numeral[] numeralArray = {Numeral.one, Numeral.two, Numeral.three, Numeral.four};
-    	Numeral[] numeralArray1 = {Numeral.two, Numeral.two, Numeral.three, Numeral.four};
-    	Barcode barcode1 = new Barcode(numeralArray);
-    	Barcode barcode2 = new Barcode(numeralArray1);
-        BarcodedProduct product1 = new BarcodedProduct(barcode1, "Product1", 1000, 10.0);
-        BarcodedProduct product2 = new BarcodedProduct(barcode2, "Product2", 2000, 15.0);
+	        // Add products to the cart
+	        cart.addBarcodedProductToCart(product1);
+	     
 
-        // Add products to the cart
-        cart.addBarcodedProductToCart(product1);
-        cart.addBarcodedProductToCart(product2);
+	        // Ensure the initial state is correct
+	        assertEquals(1, cart.getNumberItems());
+	        assertEquals(10.0, cart.getMass(), 0.001);
+	        assertEquals(1000, cart.getCartTotal(), 0.001);
+	        cart.canChange();
+	        cart.endPayment();
+	        
 
-        // Ensure the initial state is correct
-        assertEquals(2, cart.getNumberItems());
-        assertEquals(25.0, cart.getMass(), 0.001);
-        assertEquals(25.0, cart.getCartTotal(), 0.001);
+	        // Remove product1 from the cart
+	        cart.removeBarcodedProductFromCart(product1);
 
-        // Remove product1 from the cart
-        cart.removeBarcodedProductFromCart(product1);
+	        // Ensure the product is removed correctly
+	        assertEquals(0, cart.getNumberItems());
+	        assertEquals(0, cart.getMass(), 0.001);
+	        assertEquals(0, cart.getCartTotal(), 0.001);
+	    }
 
-        // Ensure the product is removed correctly
-        assertEquals(1, cart.getNumberItems());
-        assertEquals(15.0, cart.getMass(), 0.001);
-        assertEquals(15.0, cart.getCartTotal(), 0.001);
-    }
+	   @Test
+	    public void testRemoveBarcodedProductFromEmptyCart() {
+	        // Test removing a product from an empty cart
+	    	Numeral[] numeralArray = {Numeral.three, Numeral.two, Numeral.one, Numeral.four};
+	    	Barcode barcode1 = new Barcode(numeralArray);
+	        BarcodedProduct product = new BarcodedProduct(barcode1, "Product3", 2000, 20.0);
+	        cart = new Cart();
+	        cart.canChange();
+	        cart.endPayment();
+	        
 
-    @Test
-    public void testRemoveBarcodedProductFromEmptyCart() {
-        // Test removing a product from an empty cart
-    	Numeral[] numeralArray = {Numeral.three, Numeral.two, Numeral.one, Numeral.four};
-    	Barcode barcode1 = new Barcode(numeralArray);
-        BarcodedProduct product = new BarcodedProduct(barcode1, "Product3", 2000, 20.0);
+	        // Ensure the initial state is correct
+	        assertEquals(0, cart.getNumberItems());
+	        assertEquals(0.0, cart.getMass(), 0.001);
+	        assertEquals(0.0, cart.getCartTotal(), 0.001);
 
-        // Ensure the initial state is correct
-        assertEquals(0, cart.getNumberItems());
-        assertEquals(0.0, cart.getMass(), 0.001);
-        assertEquals(0.0, cart.getCartTotal(), 0.001);
+	        // Remove product from the empty cart
+	        cart.removeBarcodedProductFromCart(product);
 
-        // Remove product from the empty cart
-        cart.removeBarcodedProductFromCart(product);
+	        // Ensure the state remains unchanged
+	        assertEquals(0, cart.getNumberItems());
+	        assertEquals(0.0, cart.getMass(), 0.001);
+	        assertEquals(0.0, cart.getCartTotal(), 0.001);
+	    }
 
-        // Ensure the state remains unchanged
-        assertEquals(0, cart.getNumberItems());
-        assertEquals(0.0, cart.getMass(), 0.001);
-        assertEquals(0.0, cart.getCartTotal(), 0.001);
-    }
-    
-    @Test
-    public void testRemoveMultipleBarcodedProductsFromCart() {
-        // Test removing multiple BarcodedProducts from a non-empty cart
-        Numeral[] numeralArray1 = {Numeral.one, Numeral.two, Numeral.three, Numeral.four};
-        Numeral[] numeralArray2 = {Numeral.two, Numeral.two, Numeral.three, Numeral.four};
-        Barcode barcode1 = new Barcode(numeralArray1);
-        Barcode barcode2 = new Barcode(numeralArray2);
-        BarcodedProduct product1 = new BarcodedProduct(barcode1, "Product1", 1000, 10.0);
-        BarcodedProduct product2 = new BarcodedProduct(barcode2, "Product2", 2000, 15.0);
+	    @Test
+	    public void testRemoveBarcodedProductFromCartWhenCannotChange() {
+	        // Test removing a product when the cart cannot be changed
+	    	Numeral[] numeralArray = {Numeral.three, Numeral.two, Numeral.one, Numeral.four};
+	    	Barcode barcode1 = new Barcode(numeralArray);
+	        BarcodedProduct product = new BarcodedProduct(barcode1, "Product4", 2500, 25.0);
+	        cart = new Cart();
 
-        // Add products to the cart
-        cart.addBarcodedProductToCart(product1);
-        cart.addBarcodedProductToCart(product2);
+	        
 
-        // Ensure the initial state is correct
-        assertEquals(2, cart.getNumberItems());
-        assertEquals(25.0, cart.getMass(), 0.001);
-        assertEquals(25.0, cart.getCartTotal(), 0.001);
+	        // Set cart state to cannot change
+	       
 
-        // Remove both products from the cart
-        cart.removeBarcodedProductFromCart(product1);
-        cart.removeBarcodedProductFromCart(product2);
+	        // Add product to the cart
+	        cart.addBarcodedProductToCart(product);
 
-        // Ensure the cart is empty
-        assertEquals(0, cart.getNumberItems());
-        assertEquals(0.0, cart.getMass(), 0.001);
-        assertEquals(0.0, cart.getCartTotal(), 0.001);
-    }
+	        // Ensure the initial state is correct
+	        assertEquals(1, cart.getNumberItems());
+	        assertEquals(25.0, cart.getMass(), 0.001);
+	        assertEquals(2500.0, cart.getCartTotal(), 0.001);
 
+	        cart.cantChange();
+	        // Try to remove product when cannot change
+	        cart.removeBarcodedProductFromCart(product);
 
-    @Test
-    public void testRemoveBarcodedProductFromCartWhenCannotChange() {
-        // Test removing a product when the cart cannot be changed
-    	Numeral[] numeralArray = {Numeral.three, Numeral.two, Numeral.one, Numeral.four};
-    	Barcode barcode1 = new Barcode(numeralArray);
-        BarcodedProduct product = new BarcodedProduct(barcode1, "Product4", 2500, 25.0);
-
-        // Set cart state to cannot change
-        cart.cantChange();
-
-        // Add product to the cart
-        cart.addBarcodedProductToCart(product);
-
-        // Ensure the initial state is correct
-        assertEquals(1, cart.getNumberItems());
-        assertEquals(2.5, cart.getMass(), 0.001);
-        assertEquals(25.0, cart.getCartTotal(), 0.001);
-
-        // Try to remove product when cannot change
-        cart.removeBarcodedProductFromCart(product);
-
-        // Ensure the state remains unchanged
-        assertEquals(1, cart.getNumberItems());
-        assertEquals(2.5, cart.getMass(), 0.001);
-        assertEquals(25.0, cart.getCartTotal(), 0.001);
-    }
-    
-    @Test
-    public void testRemoveNonBarcodedProductFromCart() {
-        // Test removing a non-barcoded product from a non-empty cart
-    	Numeral[] numeralArray = {null};
-    	Barcode barcode1 = new Barcode(numeralArray);
-        BarcodedProduct nonBarcodedProduct = new BarcodedProduct(barcode1, "Product4", 2500, 25.0);
-
-        // Add a non-barcoded product to the cart
-        cart.addBarcodedProductToCart(nonBarcodedProduct);
-
-        // Ensure the initial state is correct
-        assertEquals(1, cart.getNumberItems());
-        assertEquals(12.0, cart.getMass(), 0.001);
-        assertEquals(12.0, cart.getCartTotal(), 0.001);
-
-        // Remove the non-barcoded product from the cart
-        cart.removeBarcodedProductFromCart(nonBarcodedProduct);
-
-        // Ensure the cart is empty
-        assertEquals(0, cart.getNumberItems());
-        assertEquals(0.0, cart.getMass(), 0.001);
-        assertEquals(0.0, cart.getCartTotal(), 0.001);
-    }
-    
-    
-    @Test
-    public void testCartTotalAfterRemovingProduct() {
-        // Test cart total calculation after removing a product
-        Numeral[] numeralArray = {Numeral.one, Numeral.two, Numeral.three, Numeral.four};
-        Barcode barcode = new Barcode(numeralArray);
-        BarcodedProduct product1 = new BarcodedProduct(barcode, "Product1", 1000, 10.0);
-        BarcodedProduct product2 = new BarcodedProduct(barcode, "Product2", 2000, 15.0);
-
-        // Add products to the cart
-        cart.addBarcodedProductToCart(product1);
-        cart.addBarcodedProductToCart(product2);
-
-        // Ensure the initial state is correct
-        assertEquals(2, cart.getNumberItems());
-        assertEquals(25.0, cart.getMass(), 0.001);
-        assertEquals(25.0, cart.getCartTotal(), 0.001);
-
-        // Remove product1 from the cart
-        cart.removeBarcodedProductFromCart(product1);
-
-        // Ensure the cart total is updated correctly
-        assertEquals(1, cart.getNumberItems());
-        assertEquals(15.0, cart.getMass(), 0.001);
-        assertEquals(15.0, cart.getCartTotal(), 0.001);
-    }
-
-
-   
+	        // Ensure the state remains unchanged
+	        assertEquals(1, cart.getNumberItems());
+	        assertEquals(25.0, cart.getMass(), 0.001);
+	        assertEquals(2500.0, cart.getCartTotal(), 0.001);
+	    }
+	 
+	
+	
+	
 }
